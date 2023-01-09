782e1daada7b4e1b1ead4e714a61272bebc49fa3
KrisEnigma <krisescobarm@gmail.com>
2023-01-09 09:00:32 -0300

    update

--- lib/levelbase.dart
+++ lib/levelbase.dart
@@ -66,6 +66,36 @@ class LevelWidgetState extends State<LevelWidget> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text(widget.title),
+        actions: [
+          PopupMenuButton(
+              // add icon, by default "3 dot" icon
+              icon: const Icon(Icons.settings),
+              itemBuilder: (context) {
+                return [
+                  const PopupMenuItem<int>(
+                    value: 0,
+                    child: Text("My Account"),
+                  ),
+                  const PopupMenuItem<int>(
+                    value: 1,
+                    child: Text("Settings"),
+                  ),
+                  const PopupMenuItem<int>(
+                    value: 2,
+                    child: Text("Logout"),
+                  ),
+                ];
+              },
+              onSelected: (value) {
+                if (value == 0) {
+                  print("My account menu is selected.");
+                } else if (value == 1) {
+                  print("Settings menu is selected.");
+                } else if (value == 2) {
+                  print("Logout menu is selected.");
+                }
+              }),
+        ],
       ),
       drawer: Drawer(
         child: ListView(
